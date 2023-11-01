2ae25b8b7c33638fc22a88102c8a3f26ce55152a
spaic <spaic@MANO>
2023-11-01 08:18:39 +0200

    Add project files.

---
+++ EBookMark_ISP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EBookMark_ISP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
