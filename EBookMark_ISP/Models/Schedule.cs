624f205eabeb29f4c59d01344f5effbfd9c58ab7
aleksas753 <aleksas.juzukonis@ktu.edu>
2023-11-05 00:58:27 +0200

    a

---
+++ EBookMark_ISP/Models/Schedule.cs
@@ -0,0 +1,27 @@
+﻿namespace EBookMark_ISP.Models
+{
+    public class Schedule
+    {
+        public int ScheduleId { get; set; }
+        public DateTime SemestarStart { get; set; }
+        public DateTime SemestarEnd { get; set; }
+
+        public string className { get; set; }
+
+        public List<LessonTime> Lessons;
+
+        public Schedule(int id, DateTime semestarStart, DateTime semestarEnd, string className)
+        {
+            id = ScheduleId;
+            SemestarStart = semestarStart;
+            SemestarEnd = semestarEnd;
+            this.className = className;
+            Lessons = new List<LessonTime>();
+        }
+
+        public void Add(LessonTime time)
+        {
+            Lessons.Add(time);
+        }
+    }
+}
