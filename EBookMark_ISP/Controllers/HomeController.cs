b8e287ee58068980ba405e19a5cc84a425804e22
ugnspa <125737106+ugnspa@users.noreply.github.com>
2023-11-06 02:41:50 +0200

    Added Teacher, User controllers. made Prototype sub-system for mark writting. Added Account where user can change his password (prototype). Moved schedules button for admin from navbar to admin partial view.

--- EBookMark_ISP/Controllers/HomeController.cs
+++ EBookMark_ISP/Controllers/HomeController.cs
@@ -16,9 +16,9 @@ namespace EBookMark_ISP.Controllers
         public IActionResult Index()
         {
             string username = HttpContext.Session.GetString("Username");
-            if(username != null) 
-            {
-                View("Dashboard");
+            if(username != null)
+            { 
+                return RedirectToAction("Dashboard");
             }
             string errorMessage = TempData["ErrorMessage"] as string;
             if (errorMessage != null)
