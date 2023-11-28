2d34b34d9419d95fb3c401bbf3f6f00bfda45902
Vytautas Jaras <vjaras202@gmail.com>
2023-11-28 23:48:29 +0200

    Added connection string connecting entity framework to the database.

--- EBookMark_ISP/Program.cs
+++ EBookMark_ISP/Program.cs
@@ -1,9 +1,18 @@
+using EBookMark_ISP.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddSession();
+
+builder.Services.AddDbContext<EbookmarkContext>(options =>
+{
+    options.UseMySQL("Server=localhost;Database=ebookmark;User Id=root;Password=root;"); // Replace with your actual database connection string
+});
 
 var app = builder.Build();
 
