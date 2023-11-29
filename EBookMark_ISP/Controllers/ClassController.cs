00e43e5aa2eb836c158ae5c9f84893dd2b57333d
Vytautas Jaras <vjaras202@gmail.com>
2023-11-29 17:41:14 +0200

    Aded CRUD for classes.

--- EBookMark_ISP/Controllers/ClassController.cs
+++ EBookMark_ISP/Controllers/ClassController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using EBookMark_ISP.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EBookMark_ISP.Controllers
 {
     public class ClassController : Controller
     {
+
+        private readonly EbookmarkContext _context;
+
+        public ClassController(EbookmarkContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             string username = HttpContext.Session.GetString("Username");
@@ -13,6 +23,7 @@ namespace EBookMark_ISP.Controllers
                 return RedirectToAction("Dashboard", "Home");
             }
             ViewBag.Permissions = permissions;
+            ViewBag.Classes = _context.Classes.ToList();
             return View();
         }
         
@@ -36,6 +47,23 @@ namespace EBookMark_ISP.Controllers
             {
                 return RedirectToAction("Dashboard", "Home");
             }
+
+
+            if(permissions == 1)
+            {
+                Console.WriteLine("HIIIIIIIIIIIIIIIIIIIIIIII");
+                code = _context.Students.Include(s => s.FkUserNavigation).
+                    FirstOrDefault(s => s.FkUserNavigation.Username == username).FkClass;
+                Console.WriteLine(code);
+            }
+
+            Class filteredClass = _context.Classes.Where(c => c.Code == code).FirstOrDefault();
+
+            List<Student> students = _context.Students.Where(s => s.FkClass == code).ToList();
+
+            ViewBag.Class = filteredClass;
+            ViewBag.Students = students;
+
             return View();
         }
         public IActionResult Create() 
@@ -54,11 +82,31 @@ namespace EBookMark_ISP.Controllers
         {
             string username = HttpContext.Session.GetString("Username");
             int? permissions = HttpContext.Session.GetInt32("Permissions");
+
             if (username == null || permissions == null || permissions < 9)
             {
                 return RedirectToAction("Dashboard", "Home");
             }
+
+            string code = GenerateClassCode(name, year);
+            Class newClass = new Class
+            {
+                Code = code,
+                Name = name,
+                StudentsCount = 0,
+                Year = year
+            };
+
+            _context.Classes.Add(newClass);
+            _context.SaveChanges();
+            
             return RedirectToAction("Index", "Class");
+        }
+
+        private string GenerateClassCode(string name, int year)
+        {
+            string code = year.ToString() + name + DateTime.Now.Year.ToString();
+            return code;
         }
 
         [HttpGet]
@@ -70,6 +118,25 @@ namespace EBookMark_ISP.Controllers
             {
                 return RedirectToAction("Dashboard", "Home");
             }
+
+            Console.WriteLine("Remove:");
+            Console.WriteLine($"Code {code}");
+
+            List<Student> classStudents = _context.Students.Where(s => s.FkClass == code).ToList();
+
+            foreach(var student in classStudents)
+            {
+                student.FkClass = null;
+            }
+
+            Class classToRemove = _context.Classes.FirstOrDefault(c => c.Code == code);
+            if (classToRemove != null)
+            {
+                _context.Classes.Remove(classToRemove);
+            }
+
+            _context.SaveChanges();
+
             return RedirectToAction("Index", "Class");
         }
 
@@ -93,18 +160,47 @@ namespace EBookMark_ISP.Controllers
             {
                 return RedirectToAction("Dashboard", "Home");
             }
+
+
+            Class filteredClass = _context.Classes.Where(c => c.Code == code).FirstOrDefault();
+
+            List<Student> classStudents = _context.Students.Where(s => s.FkClass == code).ToList();
+
+            List<Student> studentsToAdd = _context.Students.Where(s => s.FkClass == null).ToList();
+
+            ViewBag.Class = filteredClass;
+            ViewBag.Students = classStudents;
+            ViewBag.StudentsToAdd = studentsToAdd;
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Modify(string name, int studentCount, int year, string code)
         {
+            Console.WriteLine("Modify post:");
+            Console.WriteLine($"Name {name}");
+            Console.WriteLine($"Count {studentCount}");
+            Console.WriteLine($"Year {year}");
+            Console.WriteLine($"Code {code}");
+
             string username = HttpContext.Session.GetString("Username");
             int? permissions = HttpContext.Session.GetInt32("Permissions");
             if (username == null || permissions == null || permissions < 9)
             {
                 return RedirectToAction("Dashboard", "Home");
             }
+
+            Class filteredClass = _context.Classes.FirstOrDefault(c => c.Code == code);
+
+            if(filteredClass != null)
+            {
+                filteredClass.Name = name;
+                filteredClass.StudentsCount = studentCount;
+                filteredClass.Year = year;
+                _context.SaveChanges();
+            }
+
             return RedirectToAction("Index", "Class");
         }
 
@@ -120,15 +216,52 @@ namespace EBookMark_ISP.Controllers
         }
 
         [HttpGet]
-        public IActionResult AddStudentToClass(string id)
+        public IActionResult AddStudentToClass(string classCode, int studentId)
         {
+            Console.WriteLine($"class code: {classCode}");
+            Console.WriteLine($"student ID: {studentId}");
             string username = HttpContext.Session.GetString("Username");
             int? permissions = HttpContext.Session.GetInt32("Permissions");
             if (username == null || permissions == null || permissions < 9)
             {
                 return RedirectToAction("Dashboard", "Home");
             }
-            return RedirectToAction("Modify", "Class");
+
+            Student student = _context.Students.FirstOrDefault(s => s.FkUser == studentId);
+            if(student != null)
+            {
+                _context.Classes.FirstOrDefault(c => c.Code == classCode).StudentsCount++;
+                student.FkClass = classCode;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Modify", "Class", new {code = classCode});
+        }
+
+        [HttpGet]
+        public IActionResult RemoveStudentFromClass(string classCode, int studentId)
+        {
+            
+            string username = HttpContext.Session.GetString("Username");
+            int? permissions = HttpContext.Session.GetInt32("Permissions");
+            if (username == null || permissions == null || permissions < 9)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+
+            Console.WriteLine("Remove student from class:");
+            Console.WriteLine($" class code: {classCode}");
+            Console.WriteLine($" student ID: {studentId}");
+
+            Student student = _context.Students.FirstOrDefault(s => s.FkUser == studentId);
+            if (student != null)
+            {
+                _context.Classes.FirstOrDefault(c => c.Code == classCode).StudentsCount--;
+                student.FkClass = null;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Modify", "Class", new { code = classCode });
         }
         //[HttpPost]
         //public IActionResult AddStudentToClass(string id)
